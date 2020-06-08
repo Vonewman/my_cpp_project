29ecb0bd2bc13a498b8afc660a465dc8257a605f
vonewman <vonewman7@gmail.com>
2020-06-08 18:45:41 -0400

    add ./header_guards folder

---
+++ header_guards/main.cpp
@@ -0,0 +1,11 @@
+#include "square.h"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "a square has " << getSquareSides() << " sides\n";
+    std::cout << "a square of length 5 has perimeter length " << getSquarePerimeter(5) << '\n';
+
+
+    return 0;
+}
